fd24f5200312fd7cbb18a24ea7940d4d2772766c
Jocky-Jazz <96014317+Jocky-Jazz@users.noreply.github.com>
2026-07-23 02:09:18 +0530

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    validops = {'+', '-', '*', '/'}
+    def evalRPN(self, tokens: List[str]) -> int:
+        values = []
+        for n in tokens:
+            if (n in Solution.validops):
+                a = values.pop()
+                b = values.pop()
+                match n:
+                    case '+':
+                        values.append(b+a)
+                    case '-':
+                        values.append(b-a)
+                    case '/':
+                        if (a*b<0):
+                            values.append(-(abs(b)//abs(a)))
+                        else:
+                            values.append(abs(b)//abs(a))
+                    case '*':
+                        values.append(b*a)
+            else:
+                values.append(int(n))
+        return values[0]
