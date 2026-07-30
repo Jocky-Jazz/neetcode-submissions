3aa14720647176d0674b5d6ce0106907acde15fb
Jocky-Jazz <96014317+Jocky-Jazz@users.noreply.github.com>
2026-07-31 00:14:12 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if (len(s2) < len(s1)):
+            return False
+        string = deque([])
+        s1count = Counter(s1)
+        count = dict()
+        for ch in s2:
+            if (count == s1count):
+                return True
+            if (ch in s1):
+                string += ch
+                count[ch] = count.get(ch, 0)+1
+                while(string and count[ch] > s1count[ch]):
+                    count[string.popleft()] -= 1
+            else:
+                string.clear()
+                count.clear()
+        return count == s1count
